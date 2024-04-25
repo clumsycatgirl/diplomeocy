ed40112af25f24dd83e0652263b73b6f06cc2b1b
Ivys <127042112+michelon-eric@users.noreply.github.com>
2024-04-25 15:44:51 +0200

    chat quasi-working

--- Diplomeocy/Web/Hubs/ChatHub.cs
+++ Diplomeocy/Web/Hubs/ChatHub.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Text.Json;
+
+using Microsoft.AspNetCore.SignalR;
 
 namespace Web.Hubs;
 
 public class ChatHub : Hub {
-	public async Task SendMessage(string user, string message) {
-		Clients.All.SendAsync("ReceiveMessage", user, message);
+	public class MessageData {
+		public enum MessageDataAction { JoinGroup, LeaveGroup, SendMessage, }
+
+		public MessageDataAction Action { get; set; }
+		public string Group { get; set; }
+		public string Sender { get; set; }
+		public string? Message { get; set; }
+	}
+
+	public Task JoinGroup(string json) {
+		MessageData? data = JsonSerializer.Deserialize<MessageData>(json);
+		if (data is null || data.Action != MessageData.MessageDataAction.JoinGroup) return Task.CompletedTask;
+		return Groups.AddToGroupAsync(Context.ConnectionId, data.Group);
+	}
+
+	public Task LeaveGroup(string json) {
+		MessageData? data = JsonSerializer.Deserialize<MessageData>(json);
+		if (data is null || data.Action != MessageData.MessageDataAction.JoinGroup) return Task.CompletedTask;
+		return Groups.RemoveFromGroupAsync(Context.ConnectionId, data.Group);
+	}
+
+	public Task SendMessageToGroup(string json) {
+		MessageData? data = JsonSerializer.Deserialize<MessageData>(json);
+		if (data is null || data.Action != MessageData.MessageDataAction.SendMessage) return Task.CompletedTask;
+		return Clients.Group(data.Group).SendAsync("ReceiveMessage", data.Sender, data.Message);
 	}
 }
--- Diplomeocy/Web/wwwroot/js/chat.js
+++ Diplomeocy/Web/wwwroot/js/chat.js
@@ -1,31 +1,95 @@
 /// <reference types="@microsoft/signalr" />
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/chathub")
+    .withUrl('/chathub')
     .build();
-const sendButton = document.getElementById("send-button");
+class MessageData {
+    constructor(_action = 'JoinGroup', _group = '', _sender = '', _message = null) {
+        this._action = _action;
+        this._group = _group;
+        this._sender = _sender;
+        this._message = _message;
+    }
+    get action() {
+        return this._action;
+    }
+    set action(value) {
+        this._action = value;
+    }
+    get group() {
+        return this._group;
+    }
+    set group(value) {
+        this._group = value;
+    }
+    get sender() {
+        return this._sender;
+    }
+    set sender(value) {
+        this._sender = value;
+    }
+    get message() {
+        return this._message;
+    }
+    set message(value) {
+        this._message = value;
+    }
+}
+const sendButton = document.getElementById('send-button');
 sendButton.disabled = true;
-const messageList = document.getElementById("messages-list");
-connection.on("ReceiveMessage", (user, message) => {
-    const li = document.createElement("li");
+const joinGroupButton = document.getElementById('join-group');
+joinGroupButton.disabled = true;
+const messageList = document.getElementById('messages-list');
+connection.on('ReceiveMessage', (user, message) => {
+    const li = document.createElement('li');
     li.textContent = `${user}: ${message}`;
     messageList.appendChild(li);
 });
+let group;
 connection
     .start()
     .then(() => {
     sendButton.disabled = false;
+    joinGroupButton.disabled = false;
+    group = 'all';
+    const data = new MessageData();
+    data.action = 'JoinGroup';
+    connection.invoke('JoinGroup', JSON.stringify(data));
 })
     .catch((reason) => {
     console.error(reason.toString());
 });
-const userInput = document.getElementById("user-input");
-const messageInput = document.getElementById("message-input");
+const userInput = document.getElementById('user-input');
+const messageInput = document.getElementById('message-input');
 sendButton.onclick = (event) => {
     const user = userInput.value;
     const message = messageInput.value;
-    connection.invoke("SendMessage", user, message).catch((reason) => {
-        console.error(reason.toString());
+    const data = {
+        Group: group,
+        Sender: user,
+        Message: message,
+    };
+    connection
+        .invoke('SendMessageToGroup', JSON.stringify(data))
+        .catch((reason) => {
+        console.log(reason.toString());
     });
     event.preventDefault();
 };
+const joinGroupInput = document.getElementById('join-group-input');
+joinGroupButton.onclick = (_) => __awaiter(this, void 0, void 0, function* () {
+    if (joinGroupInput.value === '')
+        return;
+    yield connection.invoke('LeaveGroup', JSON.stringify({ Group: group }));
+    group = joinGroupInput.value;
+    yield connection.invoke('JoinGroup', JSON.stringify({ Group: group }));
+});
 //# sourceMappingURL=chat.js.map
