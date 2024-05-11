baf5c140afdd2530082b3ce58894b6546efa798b
Elena261221 <123748648+Elena261221@users.noreply.github.com>
2024-05-11 12:55:53 +0200

    meow meow indigo

--- Diplomeocy/Web/Models/User.cs
+++ Diplomeocy/Web/Models/User.cs
@@ -7,4 +7,5 @@ public class User
     public string Surname { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+	public int IdCharacter { get; set; }
 }
