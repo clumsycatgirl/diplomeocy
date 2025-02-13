c1318a0bee01a7a98cb59bd297cd4be467a3564a
michelon-eric <eric00michelon@gmail.com>
2025-02-13 13:50:34 +0100

    trying to dockerise this shit

---
+++ Web/Scripts/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+
+function getEntries(srcPath, relativePath = '.') {
+  const files = fs.readdirSync(srcPath);
+  const entries = {};
+
+  files.forEach((file) => {
+    const filePath = path.resolve(srcPath, file);
+    const relativeFilePath = path.relative(srcPath, filePath);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      Object.assign(entries, getEntries(filePath, path.join(relativePath, file)));
+    } else if (file.endsWith('.ts') || file.endsWith('.tsx')) {
+      const name = path.join(relativePath, file).replace(/\.(ts|tsx)$/, '');
+      entries[name] = filePath;
+    }
+  });
+
+  return entries;
+}
+
+module.exports = {
+  entry: getEntries('./src'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../wwwroot/js'),
+  },
+  optimization: {
+    minimize: false,
+  },
+};
