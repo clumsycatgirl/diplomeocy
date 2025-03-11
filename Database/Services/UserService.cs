272cb13e5f4dea55fec56e0055af8addffef82be
Ivysmnope <eric00michelon@gmail.com>
2025-03-11 01:22:58 +0100

    finished cleaning from the tests

--- Database/Services/UserService.cs
+++ Database/Services/UserService.cs
@@ -1,72 +1,5 @@
-﻿//using Diplomeocy.Database.Models;
-//using Diplomeocy.Extensions;
-//using Diplomeocy.Web.Exceptions;
-
-//namespace Diplomeocy.Database.Services;
-
-//public class UserService {
-//	private readonly IHttpContextAccessor httpContextAccessor = default!;
-
-//	public UserService(IHttpContextAccessor httpContextAccessor) {
-//		this.httpContextAccessor = httpContextAccessor;
-//	}
-
-//	public User? CurrentUser {
-//		get =>
-//			httpContextAccessor.HttpContext?.Session?.Get<User>("CurrentUser");
-//		set {
-//			ISession? session = httpContextAccessor.HttpContext?.Session;
-//			if (session is null) {
-//				return;
-//			}
-
-//			if (value is not null) {
-//				session.Set("CurrentUser", value);
-//			} else {
-//				session.Remove("CurrentUser");
-//			}
-//		}
-//	}
-
-//	public void RequireAuthentication(string? redirectTo = null) {
-//		if (CurrentUser is null) {
-//			throw new RedirectException(redirectTo ?? "/Auth");
-//		}
-//	}
-//}
-
-
-// using Diplomeocy.Database.Models;
-// using Diplomeocy.Database.Models.SessionModels;
-// using Diplomeocy.Web.Exceptions;
-
-// namespace Diplomeocy.Database.Services;
-
-// public class UserService {
-// 	private readonly IHttpContextAccessor httpContextAccessor;
-// 	private SessionUser? user = null;
-
-// 	public UserService(IHttpContextAccessor httpContextAccessor) {
-// 		this.httpContextAccessor = httpContextAccessor;
-// 	}
-
-// 	public SessionUser? CurrentUser => user;
-
-// 	public void SetUser(User? user) {
-// 		this.user = new SessionUser(httpContextAccessor, "CurrentUser");
-// 		this.user.Value = user;
-// 	}
-
-// 	public void RequireAuthentication(string? redirectTo = null) {
-// 		if (CurrentUser is null) {
-// 			throw new RedirectException(redirectTo ?? "/Auth");
-// 		}
-// 	}
-// }
-
-
+﻿
 using Diplomeocy.Database.Models;
-using Diplomeocy.Database.Models.SessionModels;
 using Diplomeocy.Extensions;
 using Diplomeocy.Web.Exceptions;
 
@@ -93,6 +26,7 @@ public class UserService {
 				user.PropertyChanged += (sender, args) => {
 					httpContextAccessor.HttpContext?.Session.Set<User>("CurrentUser", CurrentUser!);
 					databaseContext.Users.Update(CurrentUser!);
+					databaseContext.SaveChanges();
 				};
 			}
 			return user;
